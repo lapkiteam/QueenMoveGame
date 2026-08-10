410e2f81dea123efac3ca32e28fa7b85bb9be311
gretmn102 <erlgreywatson@gmail.com>
2026-08-11 02:11:12 +0300

    wip: нарисовать сетку

---
+++ src/components/Field.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { concat } from "../lib/utils"
+  const colsCount = 4
+  const rowsCount = 8
+</script>
+
+<div
+  class={concat([
+    "grid",
+    "size-full",
+    "aspect-square",
+    "gap-1",
+  ])}
+  style={`grid-template-columns: repeat(${colsCount}, minmax(0, 1fr))`}
+>
+  {#each new Array(colsCount * rowsCount) as _, index}
+    <div class={concat([
+      "bg-gray-500",
+      "aspect-square",
+    ])}>
+      <div>i: {index}</div>
+      <div>({index % colsCount}, {index / colsCount | 0})</div>
+    </div>
+  {/each}
+</div>

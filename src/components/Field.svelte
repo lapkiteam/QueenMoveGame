a3eededb057499275aabea2469376a1133e0c562
gretmn102 <erlgreywatson@gmail.com>
2026-08-11 02:38:42 +0300

    wip

--- src/components/Field.svelte
+++ src/components/Field.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { concat } from "../lib/utils"
-  const colsCount = 4
+  import { ElementId, Field, FieldElement } from "../lib/field"
+  const colsCount = 8
   const rowsCount = 8
+  let field = Field.create(colsCount, rowsCount)
 </script>
 
 <div
@@ -13,13 +15,43 @@
   ])}
   style={`grid-template-columns: repeat(${colsCount}, minmax(0, 1fr))`}
 >
-  {#each new Array(colsCount * rowsCount) as _, index}
-    <div class={concat([
-      "bg-gray-500",
-      "aspect-square",
-    ])}>
-      <div>i: {index}</div>
-      <div>({index % colsCount}, {index / colsCount | 0})</div>
-    </div>
+  {#each field.field as rows, y}
+    {#each rows as value, x}
+      <button
+        class={concat([
+          "bg-gray-500",
+          "aspect-square",
+          "grid-flow-row",
+          value ? (
+            "bg-white-900"
+          ) : (
+            Field.getIntersections(field, { x, y }).length === 0 ? (
+              "bg-yellow-100"
+            ) : (
+              Field.getIntersections(field, { x, y }).length === 1 ? (
+                "bg-blue-500"
+              ) : (
+                "bg-red-500"
+              )
+            )
+          ),
+
+        ])}
+        on:click={_ => {
+          field = Field.update(
+            field,
+            { x, y },
+            element => element ? (
+              FieldElement.create()
+            ) : (
+              FieldElement.create(ElementId.create())
+            )
+          )
+        }}
+      >
+        <div>{x}, {y}</div>
+        <div>{Field.getIntersections(field, { x, y })}</div>
+      </button>
+    {/each}
   {/each}
 </div>

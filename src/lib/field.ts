78980de01a85e24ded1740753330ec211577a044
Agent Lapki <treesortbubble@gmail.com>
2026-08-11 01:32:42 +0300

    feat: make field module (#1)

---
+++ src/lib/field.ts
@@ -0,0 +1,217 @@
+import { Option, UnionCase } from "@fering-org/functional-helper"
+import immutableUpdate from "immutability-helper"
+
+export namespace Option2 {
+  // refactor: use `Option.get` (see. [feat(Option): add `get` function](https://github.com/gretmn102/functional-helper/issues/12))
+  export function get<T>(opt: Option<T>): T {
+    if (opt === undefined) {
+      throw new Error("Argument is undefined")
+    }
+    return opt
+  }
+}
+
+export type ElementId = string
+
+export namespace ElementId {
+  export function create(): ElementId
+  export function create(date: Date): ElementId
+  export function create(date?: Date): ElementId {
+    return (() => {
+      if (date === undefined) {
+        return new Date()
+      }
+      return date
+    })().valueOf().toString()
+  }
+}
+
+export type FieldElement = Option<ElementId>
+
+export namespace FieldElement {
+  export function create(): FieldElement
+  export function create(element: ElementId): FieldElement
+  export function create(element?: ElementId): FieldElement {
+    if (element === undefined) {
+      return Option.mkNone()
+    }
+    UnionCase.mkUnionCase
+    return Option.mkSome(element)
+  }
+
+  export function getValue(element: FieldElement): ElementId {
+    return Option2.get(element)
+  }
+}
+
+export type Position = {
+  x: number,
+  y: number,
+}
+
+export type Field = {
+  elements: Map<ElementId, Position>
+  field: FieldElement[][]
+}
+
+export namespace Field {
+  export function create(
+    width: number,
+    height: number,
+  ): Field {
+    const field = Array<FieldElement[]>(height)
+    for (let i = 0; i < field.length; i++) {
+      const xs = Array<FieldElement>(width)
+      for (let j = 0; j < xs.length; j++) {
+        xs[j] = Option.mkNone()
+      }
+      field[i] = xs
+    }
+    return {
+      elements: new Map(),
+      field,
+    }
+  }
+
+  export function update(
+    field: Field,
+    pos: Position,
+    updating: ((element: FieldElement) => FieldElement),
+  ): Field {
+    const { x, y } = pos
+    const currentElement = field.field[y][x]
+    const updatedElement = updating(currentElement)
+    const updatedField = immutableUpdate(field, {
+      field: {
+        [y]: {
+          $apply: (row: FieldElement[]) => immutableUpdate(row, {
+            [x]: {
+              $set: updatedElement
+            }
+          })
+        }
+      }
+    })
+
+    if (updatedElement === undefined) {
+      if (currentElement === undefined) {
+        return updatedField
+      }
+      return immutableUpdate(updatedField, {
+        elements: {
+          $apply: (elements: Field["elements"]) => immutableUpdate(elements, {
+            $remove: [currentElement]
+          })
+        },
+      })
+    }
+
+    const updatedField2 = (() => {
+      if (currentElement === undefined) {
+        return updatedField
+      }
+      if (currentElement === updatedElement) {
+        return updatedField
+      }
+      return immutableUpdate(updatedField, {
+        elements: {
+          $remove: [currentElement]
+        }
+      })
+    })()
+
+    return immutableUpdate(updatedField2, {
+      elements: {
+        $apply: (elements: Field["elements"]) => immutableUpdate(elements, {
+          [updatedElement]: {
+            $set: pos
+          }
+        })
+      },
+    })
+  }
+
+  export function getIntersections(
+    field: Field,
+    pos: Position,
+  ): ElementId[] {
+    const intersects = new Array<ElementId>()
+    const cols = field.field
+    function add(element: FieldElement) {
+      if (element === undefined) {
+        return
+      }
+      intersects.push(element)
+    }
+
+    // ↖ North West
+    for (let y = pos.y - 1; y >= 0; y--) {
+      const rows = cols[y]
+      for (let x = pos.x - 1; x >= 0; x--) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    // ↘ South East
+    for (let y = pos.y + 1; y < cols.length; y++) {
+      const rows = cols[y]
+      for (let x = pos.x + 1; x < rows.length; x++) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    // ↑ Upwards
+    for (let y = pos.y - 1; y >= 0; y--) {
+      const rows = cols[y]
+      const element = rows[pos.x]
+      add(element)
+    }
+
+    // ↓ Downwards
+    for (let y = pos.y + 1; y < cols.length; y++) {
+      const rows = cols[y]
+      const element = rows[pos.x]
+      add(element)
+    }
+
+    // ↗ North East
+    for (let y = pos.y - 1; y >= 0; y--) {
+      const rows = cols[y]
+      for (let x = pos.x + 1; x < rows.length; x++) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    // ↙ South West
+    for (let y = pos.y + 1; y < cols.length; y++) {
+      const rows = cols[y]
+      for (let x = pos.x - 1; x >= 0; x--) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    // ← Leftwards
+    {
+      const rows = cols[pos.y]
+      for (let x = pos.x - 1; x >= 0; x--) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    // → Rightwards
+    {
+      const rows = cols[pos.y]
+      for (let x = pos.x + 1; x < rows.length; x++) {
+        const element = rows[x]
+        add(element)
+      }
+    }
+
+    return intersects
+  }
+}
---
+++ tests/lib/field.test.ts
@@ -0,0 +1,173 @@
+import { describe, expect, it } from "vitest"
+import { Option } from "@fering-org/functional-helper"
+import immutableUpdate from "immutability-helper";
+
+import { ElementId, Field, FieldElement, Option2, Position } from "../../src/lib/field"
+
+// refactor: [feat(ArrayExt): add pick function #11](https://github.com/gretmn102/functional-helper/issues/11)
+export namespace ArrayExt {
+  export function pick<T, U>(
+    array: T[],
+    picking: (value: T, index: number) => Option<U>,
+  ): Option<U> {
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index]
+      const result = picking(element, index)
+      if (result === undefined) {
+        continue
+      }
+      return result
+    }
+    return Option.mkNone()
+  }
+}
+
+describe("Field.update", () => {
+  it("place new element", () => {
+    const elementId = ElementId.create(new Date(0))
+    const element = FieldElement.create(elementId)
+    const elementPos = { x: 1, y: 0 }
+    const width = 2
+    const height = 2
+    expect(
+      Field.update(
+        Field.create(width, height),
+        elementPos,
+        _ => element,
+      )
+    )
+      .toStrictEqual({
+        elements: new Map([[elementId, elementPos]]),
+        field: (() => {
+          const field = Field.create(width, height).field
+          field[elementPos.y][elementPos.x] = elementId
+          return field
+        })(),
+      } as Field)
+  })
+  it("remove element", () => {
+    const elementId = ElementId.create(new Date(0))
+    const element = FieldElement.create(elementId)
+    const elementPos = { x: 1, y: 0 }
+    const width = 2
+    const height = 2
+    expect(
+      Field.update(
+        Field.update(
+          Field.create(width, height),
+          elementPos,
+          _ => element,
+        ),
+        elementPos,
+        _ => Option.mkNone(),
+      )
+    )
+      .toStrictEqual({
+        elements: new Map(),
+        field: Field.create(width, height).field,
+      } as Field)
+  })
+  it("replace old element to new element", () => {
+    const element1 = FieldElement.create(
+      ElementId.create(new Date(0))
+    )
+    const element2 = FieldElement.create(
+      ElementId.create(new Date(1))
+    )
+    const element2Id = FieldElement.getValue(element2)
+    const elementPos = { x: 1, y: 0 }
+    const width = 2
+    const height = 2
+    expect(
+      Field.update(
+        Field.update(
+          Field.create(width, height),
+          elementPos,
+          _ => element1,
+        ),
+        elementPos,
+        _ => element2,
+      )
+    )
+      .toStrictEqual({
+        elements: new Map([
+          [element2Id, elementPos]
+        ]),
+        field: (() => {
+          const field = Field.create(width, height).field
+          field[elementPos.y][elementPos.x] = element2Id
+          return field
+        })(),
+      } as Field)
+  })
+})
+
+describe("Field.getIntersections", () => {
+  it("counts", () => {
+    const cols: (" " | "I" | "x")[][] = [
+      ["I", " ", "I", " "],
+      [" ", " ", " ", "I"],
+      [" ", "I", "x", "I"],
+      [" ", "I", "I", "I"],
+    ]
+
+    const field = cols.reduce(
+      (state, rows, y) => (
+        rows.reduce(
+          (state, current, x) => (
+            (current === "I") ? (
+              immutableUpdate(state, {
+                field: {
+                  $apply: field =>
+                    Field.update(
+                      field,
+                      { x, y },
+                      _ => FieldElement.create(
+                        ElementId.create(new Date(state.id))
+                      )
+                    )
+                },
+                id: {
+                  $apply: id => id + 1
+                },
+              })
+            ) : (
+              state
+            )
+          ),
+          state
+        )
+      ), {
+        field: Field.create(cols[0].length, cols.length),
+        id: 0,
+      }
+    ).field
+
+    const targetPosition = (() => {
+      const result = ArrayExt.pick(cols, (rows, y) => (
+        ArrayExt.pick(rows, (value, x) => {
+          if (value !== "x") {
+            return
+          }
+          return Option.mkSome({ x, y } as Position)
+        })
+      ))
+      return Option2.get(result)
+    })()
+
+    expect(
+      Field.getIntersections(field, targetPosition).length
+    )
+      .toStrictEqual(
+        cols.reduce(
+          (count, rows) => rows.reduce(
+            (count, value) => (
+              value !== "I" ? count : count + 1
+            ),
+            count
+          ),
+          0,
+        )
+      )
+  })
+})
